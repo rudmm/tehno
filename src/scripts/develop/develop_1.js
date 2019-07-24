35b8a5f358d8c981387fa7eb3624ac138bf3b4b9
Mykhailo Rud <rudmm@ukr.net>
2019-07-24 15:26:27 +0300

    начал делаит выпадающие окно услуги

--- src/scripts/develop/develop_1.js
+++ src/scripts/develop/develop_1.js
@@ -4,8 +4,6 @@ function topSectionSlider(){
      	dots: true,
      	fade: true,
      	speed: 1000,
-     	autoplay: true,
-     	autoplaySpeed: 3000,
      	appendDots: $('.topsection-menu-block-dots'),
      	prevArrow: '<button class="topsection-btnPrev"><svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M-3.49691e-07 7L8.25 0.0717961L8.25 13.9282L-3.49691e-07 7Z" fill="#fff"/> </svg></button>',
      	nextArrow: '<button class="topsection-btnNext"><svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 7L0.75 13.9282L0.750001 0.0717965L9 7Z" fill="#fff"/></svg></button>'
