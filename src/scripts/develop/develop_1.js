a8523583742fd9588d8f2baa6148175dac9b0f75
Mykhailo Rud <rudmm@ukr.net>
2019-07-25 16:49:06 +0300

    сделал слайдер постов

--- src/scripts/develop/develop_1.js
+++ src/scripts/develop/develop_1.js
@@ -7,14 +7,9 @@ function topSectionSlider(){
      	autoplaySpeed: 3000,
      	speed: 1000,
      	appendDots: $('.topsection-menu-block-dots'),
-     	prevArrow: '<button class="topsection-btnPrev"><svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M-3.49691e-07 7L8.25 0.0717961L8.25 13.9282L-3.49691e-07 7Z" fill="#fff"/> </svg></button>',
-     	nextArrow: '<button class="topsection-btnNext"><svg width="9" height="14" viewBox="0 0 9 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M9 7L0.75 13.9282L0.750001 0.0717965L9 7Z" fill="#fff"/></svg></button>'
+     	prevArrow: $('.slPrev'),
+     	nextArrow: $('.slNxt')
      	 });
-
-     let slickPrev = $('.slick-prev');
-     let slickNext = $('.slick-next');
-     slickPrev.html('');
-     slickNext.html('');
 }
 function openCloseServicesWindow(){
 	let menu = $('.menu');
@@ -41,10 +36,24 @@ function openCloseServicesWindow(){
 	});
 }
 
+function sectionServicesSlider(){
+	$('.sectionservices-slider').slick({
+		slidesToShow: 4,
+		dots: true,
+		appendDots: $('.sectionservices-dots'),
+		prevArrow: $('.btnPrev'),
+		nextArrow: $('.btnNxt'),
+		autoplay: true,
+		autoplaySpeed: 3000,
+		swipe: false
+
+	});
+}
 
 $(document).ready(function(){
 	topSectionSlider();
 	openCloseServicesWindow();
+	sectionServicesSlider();
 });
 $(window).load(function(){
 });
